92c2ec1b6db541dbc2c007476fa6c1b6704b1f52
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-27 18:08:23 -0300

    myself_among_others first version

--- index.js
+++ index.js
@@ -1,36 +1,64 @@
-const processador = require ('./processador')
-const titleCase = require ('title-case')
+const processador = require('./processador')
+const titleCase = require('title-case')
+const _ = require('underscore')
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hey, this is Avante! USJT")
 })
 
-app.get ("/student_status", (req, res) => {
+
+app.get('/myself_among_others', (req, res) => {
+    const getDados = (dados) => {
+        res.send(dados)
+    }
+    const aux = (consolidatedAvtCoins) => {
+        consolidatedAvtCoins = _.sortBy (consolidatedAvtCoins, o => o.avtCoins)
+        consolidatedAvtCoins = consolidatedAvtCoins.reverse()
+        const posicaoDoAluno = _.findIndex(consolidatedAvtCoins, (a) => a.ra === req.query.ra)
+        let indexOfUpLimit = posicaoDoAluno <= 5 ? 0 : posicaoDoAluno - 5
+        let indexOfBottomLimit = posicaoDoAluno >= consolidatedAvtCoins.length - 4 ? consolidatedAvtCoins.length - 1 : posicaoDoAluno + (10 - (posicaoDoAluno - indexOfUpLimit))
+        console.log(indexOfUpLimit)
+        console.log(posicaoDoAluno)
+        console.log(indexOfBottomLimit)
+        const upPortion = consolidatedAvtCoins.slice(indexOfUpLimit, posicaoDoAluno)
+        const BottomPortion = consolidatedAvtCoins.slice(posicaoDoAluno, indexOfBottomLimit)
+        theFinalWindow = [...upPortion, ...BottomPortion]
+        console.log(theFinalWindow)
+        getDados(theFinalWindow)
+    }
+    processador.consolidatedAvtCoins(aux)
+})
+
+app.get('/top_ones', (req, res) => {
+
+})
+
+app.get("/student_status", (req, res) => {
     const getDados = (dados) => {
         res.status(dados.ra !== 0 ? 200 : 404)
         res.send(dados)
     }
     const getStudentStatus = (ra, getDados) => {
         const aux = (itens) => {
-          let alunoResultado = {nome: '', ra: 0, avtCoins: 0}
-          for (let item of itens){
-            for (let aluno of item.alunos){
-              if (ra == aluno.ra){
-                alunoResultado.nome = titleCase.titleCase(aluno.nome.toString().toLowerCase())
-                alunoResultado.ra = aluno.ra
-                alunoResultado.avtCoins += +aluno.avtCoins
-              }
+            let alunoResultado = { nome: '', ra: 0, avtCoins: 0 }
+            for (let item of itens) {
+                for (let aluno of item.alunos) {
+                    if (ra == aluno.ra) {
+                        alunoResultado.nome = titleCase.titleCase(aluno.nome.toString().toLowerCase())
+                        alunoResultado.ra = aluno.ra
+                        alunoResultado.avtCoins += +aluno.avtCoins
+                    }
+                }
             }
-          }
-          getDados(alunoResultado)
+            getDados(alunoResultado)
         }
-        processador(aux)
-      }
-      getStudentStatus(req.query.ra, getDados)
+        processador.obterBaseInteiraPorData(aux)
+    }
+    getStudentStatus(req.query.ra, getDados)
 })
 
-app.listen (process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000)
 
--- processador.js
+++ processador.js
@@ -1,16 +1,16 @@
 csv = require('fast-csv')
-fs = require ('fs')
-_ = require ('underscore')
+fs = require('fs')
+_ = require('underscore')
 
-module.exports =  (getDados) => {
+const obterBaseInteiraPorData = (getDados) => {
     const avtCoinsPorPresenca = 15
     let alunos = []
     fs.createReadStream("presencas.csv")
-        .pipe(csv.parse({headers: true}))
-        .on('data', (linha) =>{ 
+        .pipe(csv.parse({ headers: true }))
+        .on('data', (linha) => {
             const date = new Date(linha['Carimbo de data/hora'])
             const diaDaSemana = date.getDay()
-            if (diaDaSemana === 3){
+            if (diaDaSemana === 3) {
                 const data = date.toLocaleDateString()
                 const aluno = {
                     nome: linha["Nome completo, sem abreviações"],
@@ -18,17 +18,43 @@ module.exports =  (getDados) => {
                     avtCoins: avtCoinsPorPresenca
                 }
                 const item = alunos.find((elemento) => elemento.data === data)
-                if (!item){
-                    alunos.push({data: data, alunos: [aluno]})
-                }else{
+                if (!item) {
+                    alunos.push({ data: data, alunos: [aluno] })
+                } else {
                     const indice = item.alunos.findIndex(a => a.ra === aluno.ra)
                     if (indice < 0)
                         item.alunos.push(aluno)
                 }
             }
         })
-        .on('end', (totalLinhas) =>{
+        .on('end', (totalLinhas) => {
             getDados(alunos)
         })
-       
+
 }
+
+const consolidatedAvtCoins = (callback) => {
+    const doIt = () => {
+        const getDados = (dados) => {
+            let alunosConsolidados = []
+            for (objeto of dados) {
+                    for (aluno of objeto.alunos) {
+                        const alunoConsolidado = alunosConsolidados.find((a) => a.ra === aluno.ra)
+                        if (alunoConsolidado) {
+                            alunoConsolidado.avtCoins += aluno.avtCoins
+                        }
+                        else {
+                            alunosConsolidados.push({ nome: aluno.nome, ra: aluno.ra, avtCoins: aluno.avtCoins })
+                        }
+                    }
+            }
+            callback(alunosConsolidados)
+        }
+        obterBaseInteiraPorData(getDados)
+    }
+    doIt()
+}
+
+
+
+module.exports = {consolidatedAvtCoins, obterBaseInteiraPorData}
